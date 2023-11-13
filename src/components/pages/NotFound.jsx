1be7001838a5348836a7c2cb29f05018d115a261
sebcod <sebastien.codus@live.fr>
2023-11-13 10:46:58 +0100

    feat:add_404_page

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,9 +1,15 @@
 //import React from 'react';
 
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div>
-      <h2>404</h2>
+    <div className="notfound">
+      <h1>404</h1>
+      <h2>Oups! La page que vous demandez n&#39;existe pas.</h2>
+      <nav>
+        <Link to={"/"}>Retourner sur la page d&#39;accueil</Link>
+      </nav>
     </div>
   );
 };
